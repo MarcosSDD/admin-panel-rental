5bea6d34c9c2807b14218a52ac2d7b3d2aad4ba7
Marcos Saldivia <marcos.saldivia@gmail.com>
2023-11-29 16:28:33 -0300

    add dashboard components and cards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashComponent } from './dash/dash.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DashComponent,
+    pathMatch: 'full'
+  },
+  {
+  path: 'dashboard',
+  component: DashComponent,
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
